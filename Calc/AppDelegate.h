d46a793119e5deac1818193e56cc41a75cd0c8f5
Igors <mahmon@inbox.lv>
2014-05-23 16:41:26 +0300

    Initial Commit

---
+++ Calc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calc
+//
+//  Created by Igors.Sivickis on 5/23/14.
+//  Copyright (c) 2014 Accenture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calc
+//
+//  Created by Igors.Sivickis on 5/23/14.
+//  Copyright (c) 2014 Accenture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
